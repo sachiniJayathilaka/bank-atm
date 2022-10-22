63511903cd604de87c252fa4b0ee39a26ac98ef1
tadsachini2@gmail.com <tadsachini2@gmail.com>
2022-10-23 00:22:56 +0530

    HSBC project created

---
+++ src/main/java/com/bank/hsbcatm/HsbcAtmApplication.java
@@ -0,0 +1,13 @@
+package com.bank.hsbcatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcAtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsbcAtmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/hsbcatm/HsbcAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.hsbcatm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsbcAtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
